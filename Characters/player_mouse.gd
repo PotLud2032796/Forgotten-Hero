518c2822b03dd65e52d6d1fbac8edaa81ce2cd33
ChuckD46 <Charlo_d46@outlook.com>
2023-10-14 19:20:44 -0400

    Adding dash ability

--- Characters/player_mouse.gd
+++ Characters/player_mouse.gd
@@ -1,29 +1,48 @@
 extends CharacterBody2D
 
-@export var max_speed : float = 100
-@export var accel : float = 1500
-@export var friction : float = 600
+# Variables exposées
+@export var max_speed : float = 125
+@export var accel : float = 1000
+@export var friction : float = 1250
+@export var dash_speed : float = 400
+@export var dash_cooldown : float = 1
 
-var input = Vector2.ZERO
+# Variables internes
+var can_dash : bool = true
+var dash_timer : Timer
+
+func _ready():
+	dash_timer = $DashTimer
 
 func _physics_process(delta):
 	player_movement(delta)
 
 func get_input():
-	input.x = int(Input.is_action_pressed("right")) -  int(Input.is_action_pressed("left"))
-	input.y = int(Input.is_action_pressed("down")) -  int(Input.is_action_pressed("up"))
+	var input = Vector2.ZERO
+	input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	input.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
 	return input.normalized()
 
 func player_movement(delta):
-	input = get_input()
-	
+	var input = get_input()
+
 	if input == Vector2.ZERO:
-		if velocity.length() > (friction * delta):
-			velocity -= velocity.normalized() * (friction * delta)
-		else:
-			velocity = Vector2.ZERO
+		var friction_force = velocity.normalized() * (friction * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, friction_force.length())
 	else:
-		velocity += (input * accel * delta)
-		velocity = velocity.limit_length(max_speed)
-	
+		velocity = velocity.move_toward(input * max_speed, accel * delta)
+
 	move_and_slide()
+
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dash()
+
+
+func dash():
+	var dash_direction = velocity.normalized()
+	velocity = dash_direction * dash_speed
+	dash_timer.start(dash_cooldown)
+	can_dash = false
+
+func _on_dash_timer_timeout():
+	can_dash = true
