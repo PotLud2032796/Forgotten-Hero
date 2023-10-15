370181d7b65da0986cd9b873d016b18f03cd8a2e
Ludovic Potvin <ludovicpotvin@hotmail.com>
2023-10-15 00:13:10 -0400

    Basic elements logic

--- Objects/Scene/Level/Levels/level_1.gd
+++ Objects/Scene/Level/Levels/level_1.gd
@@ -3,11 +3,12 @@ extends Node2D
 signal level_finished
 signal player_died
 
-func player_touched():
-	print("DEAD")
-
 func _on_finish_door_body_entered(body:Node2D):
 	if body.name == 'PlayerMouse':
 		print('mouse_reach_obj')
 		level_finished.emit()
 
+
+
+func _on_player_touched():
+	print("DEAD")
